70394e4d9e64adcd8a0b4186aa916119a9ce8d41
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-02-22 14:49:30 +0000

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use log::debug;
+use log::{debug, info, warn};
 use pretty_env_logger;
+use std::env;
 
 mod config;
 
+const DEFAULT_LOG_LEVEL: &'static str = "warn";
+
 fn main() {
+    setup_log_level_env();
     pretty_env_logger::init();
 
     let cfg = match config::get_config("../../config.toml") {
         Ok(c) => c,
         Err(e) => {
-            debug!("failed to read config");
+            warn!("failed to read config");
             debug!("error cause: {:#?}", e);
             return;
         }
@@ -17,5 +21,18 @@ fn main() {
 
     debug!("config given is:\n {:#?}", &cfg);
 
-    println!("Hello, world!");
+    todo!()
+}
+
+fn setup_log_level_env() {
+    let mut log_level = String::from(DEFAULT_LOG_LEVEL);
+
+    for (env_key, env_value) in env::vars() {
+        if env_key.eq_ignore_ascii_case("LOG_LEVEL") {
+            log_level = env_value;
+        }
+    }
+
+    env::set_var("LOG_LEVEL", &log_level);
+    info!("using LOG_LEVEL = {}", &log_level);
 }
