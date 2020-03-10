615b9f5abe362b70018f9c206c983e2d49214b4d
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-10 16:46:47 +0000

    remove db migrations

--- migrations/2020-03-02-002904_create_accounts/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE accounts (
-	uuid UUID PRIMARY KEY,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	email VARCHAR NOT NULL UNIQUE,
-	iteration_count INTEGER NOT NULL,
-	salt CHAR(16) NOT NULL,
-	password_hash CHAR(32) NOT NULL
-)
