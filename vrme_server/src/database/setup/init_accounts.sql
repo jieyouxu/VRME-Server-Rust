6f7bdfa691a31aaa377b0cd3445fb98646d95618
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-16 13:51:52 +0000

    Add setup sql queries

---
+++ vrme_server/src/database/setup/init_accounts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS accounts (
+	user_id SERIAL PRIMARY KEY,
+	email VARCHAR(355) UNIQUE NOT NULL,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	iteration_count INT NOT NULL CHECK(iteration_count >= 0),
+	salt CHAR(16) NOT NULL,
+	password_hash CHAR(32) NOT NULL
+);
---
+++ vrme_server/src/database/setup/init_database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE IF NOT EXISTS vrme_server;
