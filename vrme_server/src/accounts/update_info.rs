d75728960b14cd28e83d6d987f5b9ebf81623996
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-28 11:42:46 +0800

    Add PUT /accounts/{uuid} handler

--- vrme_server/src/accounts/update_info.rs
+++ vrme_server/src/accounts/update_info.rs
@@ -1,6 +1,9 @@
 //! Handle updating user account information.
 
-use actix_web::{web, HttpResponse};
+use crate::database::ConnectionPool;
+use crate::service_errors::ServiceError;
+use actix_web::{web, HttpResponse, ResponseError};
+use deadpool_postgres::Client;
 use serde::{Deserialize, Serialize};
 
 /// The user is only allowed to update their first name and last name.
@@ -50,6 +53,54 @@ pub struct UpdateAccountInformationRequest {
 ///
 /// If the `uuid` of a user is lost by the client, it is possible to recover the `uuid` of the user
 /// by the `GET /user/uuid` endpoint.
-pub async fn update_user_account_handler(_path: web::Path<uuid::Uuid>) -> HttpResponse {
-	unimplemented!()
+pub async fn update_user_account_handler(
+	pool: web::Data<ConnectionPool>,
+	path: web::Path<uuid::Uuid>,
+	req: web::Json<UpdateAccountInformationRequest>,
+) -> HttpResponse {
+	let client = match pool.get().await {
+		Ok(client) => client,
+		Err(e) => return e.error_response(),
+	};
+
+	match update_names(&client, &path, &req.first_name, &req.last_name).await {
+		Ok(()) => HttpResponse::Ok().finish(),
+		Err(e) => e.error_response(),
+	}
+}
+
+const UPSERT_NAME_QUERY: &str = r#"
+    INSERT INTO accounts
+        (first_name, last_name)
+    VALUES (
+        $2::VARCHAR(100),
+        $3::VARCHAR(100)
+    )
+    WHERE
+        user_id = $1::UUID
+    ON CONFLCIT (first_name, last_name)
+    DO UPDATE SET
+        first_name = COALESCE(EXCLUDED.first_name, first_name),
+        last_name =  COALESCE(EXCLUDED.last_name, last_name)
+    ;
+"#;
+
+async fn update_names(
+	client: &Client,
+	uuid: &uuid::Uuid,
+	first_name: &Option<String>,
+	last_name: &Option<String>,
+) -> Result<(), ServiceError> {
+	let statement = client.prepare(UPSERT_NAME_QUERY).await?;
+	client
+		.query(
+			&statement,
+			&[
+				&uuid,
+				first_name.as_ref().unwrap_or(&"".to_string()),
+				last_name.as_ref().unwrap_or(&"".to_string()),
+			],
+		)
+		.await?;
+	Ok(())
 }
