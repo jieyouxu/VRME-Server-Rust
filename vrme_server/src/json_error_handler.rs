89137527e2f656eb6443ebcaecd49651c0fcb49d
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-28 22:39:52 +0800

    Use semantic HTTP error codes for json errors

--- vrme_server/src/json_error_handler.rs
+++ vrme_server/src/json_error_handler.rs
@@ -1,61 +1,20 @@
-use crate::service_errors::ServiceError;
-use actix_web::error::{Error, JsonPayloadError};
-use actix_web::HttpRequest;
+use actix_web::error;
+use actix_web::{HttpRequest, HttpResponse};
 
-/// Custom JSON error handler.
+/// JSON error handler.
 ///
-/// When a JSON error is encountered, it returns a detailed error message for the malformed payload,
-/// both for syntactical malformed payload and for semantically malformed payload such as missing
-/// fields.
-///
-/// # Example Error Response
-///
-/// ```http
-/// HTTP/1.1 400 Bad Request
-/// Content-Type: application/json
-///
-/// {
-///     "cause": "bad-request",
-///     "message": "Invalid JSON at [line = 1, col = 1]"
-/// }
-/// ```
-pub fn handle_json_error(err: JsonPayloadError, _req: &HttpRequest) -> Error {
-	let err_msg = make_error_message(err);
+/// Take from: https://github.com/actix/examples/blob/master/json_decode_error/src/main.rs.
+pub fn json_error_handler(cause: error::JsonPayloadError, _req: &HttpRequest) -> error::Error {
+	use actix_web::error::JsonPayloadError;
 
-	ServiceError::BadRequest(format!("Failed to parse payload as JSON: {}", err_msg)).into()
-}
-
-fn make_error_message(err: JsonPayloadError) -> String {
-	match err {
-		JsonPayloadError::Overflow => format!("Payload size exceeds the max limit"),
-		JsonPayloadError::ContentType => {
-			"Invalid `Content-Type` header: use `application/json`".to_string()
+	let detail = cause.to_string();
+	let response = match &cause {
+		JsonPayloadError::ContentType => HttpResponse::UnsupportedMediaType().body(detail),
+		JsonPayloadError::Deserialize(json_err) if json_err.is_data() => {
+			HttpResponse::UnprocessableEntity().body(detail)
 		}
-		JsonPayloadError::Deserialize(ref e) => handle_deserialize_errors(e),
-		_ => "Invalid JSON payload".to_string(),
-	}
-}
+		_ => HttpResponse::BadRequest().body(detail),
+	};
 
-fn handle_deserialize_errors(e: &serde_json::error::Error) -> String {
-	use serde_json::error::Category;
-	match e.classify() {
-		Category::Syntax => format!(
-			"Invalid JSON at [line = {}, col = {}]",
-			e.line(),
-			e.column()
-		),
-		Category::Data => {
-			// Unfortunately `serde_json`'s Errors are opaque and do not contain useful
-			// information such as missing fields.
-			//
-			// Hence, we can only exploit it's `std::fmt::Display`'s implementation to get
-			// which field is missing that is required.
-			format!(
-				"Missing required field(s) and/or values have invalid types: {}",
-				e.to_string()
-			)
-		}
-		Category::Eof => "Expected EOF when trying to parse JSON".to_string(),
-		Category::Io => "IO error when parsing JSON".to_string(),
-	}
+	error::InternalError::from_response(cause, response).into()
 }
