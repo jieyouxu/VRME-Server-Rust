5a6f3b844ab9dcdcc9122c4ae82b2649f5c1f17e
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-29 13:51:28 +0800

    feat(auth-payload): extract construction logic for `AuthPayload`
    The `AuthPayload::from_bearer_auth` function is designed to construct a
    valid `AuthPayload` from the
    `actix-web-http-auth::extractors::bearer::BearerAuth` if and only if the
    latter contains a syntactically and semantically well-formed
    `AuthPayload` JSON such as
    
    ```json
    {
        "uuid: "d9e3a277-ce88-41eb-8f41-846e96cb461a",
        "auth_token": "MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI="
    }
    ```

--- vrme_server/src/auth/auth_payload.rs
+++ vrme_server/src/auth/auth_payload.rs
@@ -1,5 +1,7 @@
 //! Authentication payload.
 
+use crate::service_errors::ServiceError;
+use actix_web_httpauth::extractors::bearer::BearerAuth;
 use serde::{Deserialize, Serialize};
 
 /// Required authentication payload.
@@ -11,4 +13,15 @@ pub struct AuthPayload {
 	/// Authentication token issued by the server whena user successfully registered via
 	/// `POST /register` and logs in successfully via `POST /login`.
 	pub auth_token: String,
+}
+
+// Try convert `BearerAuth` into `AuthPayload` by base64-decoding token and deserializing it as the
+// `AuthPayload` json.
+impl AuthPayload {
+	fn from_bearer_auth(auth_info: &BearerAuth) -> Result<AuthPayload, ServiceError> {
+		let auth_info = auth_info.token();
+		let raw = base64::decode(auth_info)?;
+		let auth_payload = serde_json::from_slice(raw.as_slice())?;
+		Ok(auth_payload)
+	}
 }
