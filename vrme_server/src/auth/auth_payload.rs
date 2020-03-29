469a18478e27ba73e7e5fc2b3b26670390535d88
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-29 14:31:27 +0800

    fix(auth-payload): add sanity check for `auth_token` length
    Required to be `44` base64 characters after `32` bytes of raw
    `auth_token` is encoded.

--- vrme_server/src/auth/auth_payload.rs
+++ vrme_server/src/auth/auth_payload.rs
@@ -15,13 +15,33 @@ pub struct AuthPayload {
 	pub auth_token: String,
 }
 
+const INVALID_AUTH_TOKEN_LEN_ERR_MSG: &str =
+	"The `auth_token` must be base64-encoded and have a length of `44` base64 characters";
+
 // Try convert `BearerAuth` into `AuthPayload` by base64-decoding token and deserializing it as the
 // `AuthPayload` json.
 impl AuthPayload {
 	pub fn from_bearer_auth(auth_info: &BearerAuth) -> Result<AuthPayload, ServiceError> {
 		let auth_info = auth_info.token();
 		let raw = base64::decode(auth_info)?;
-		let auth_payload = serde_json::from_slice(raw.as_slice())?;
+		let auth_payload = serde_json::from_slice::<AuthPayload>(raw.as_slice())?;
+
+		if auth_payload.auth_token.len() != BASE64_ENCODED_AUTH_TOKEN_LEN {
+			return Err(ServiceError::Unauthorized(
+				INVALID_AUTH_TOKEN_LEN_ERR_MSG.to_string(),
+			));
+		}
+
 		Ok(auth_payload)
 	}
 }
+
+/// Length of an `base64`-encoded `auth_token`.
+///
+/// An `auth_token` is 32-bytes in raw binary form. To encode this as proper UTF-8 when transmitting
+/// over the network, we encode the 32 bytes with `base64` encoding.
+///
+/// The minimum `base64` characters required to encode `32` bytes is `4 * 32 / 3` which rounds up to
+/// `43`. However, `base64` requires the length to be a multiple of `4` so we round `43` up to the
+/// next multiple of `4` which is `44`.
+pub const BASE64_ENCODED_AUTH_TOKEN_LEN: usize = 44;
